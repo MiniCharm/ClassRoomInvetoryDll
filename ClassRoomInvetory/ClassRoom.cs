54675f77921abca8b95fce2d793793e6ad850d80
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-10-06 10:25:04 +0200

    implementet properties, constructor and validation in ClassRoom class

--- ClassRoomInvetory/ClassRoom.cs
+++ ClassRoomInvetory/ClassRoom.cs
@@ -8,6 +8,42 @@ namespace ClassRoomInvetory
 {
     public class ClassRoom
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Chairs { get; set; }
+
+        public int Tables {  get; set; }
+
+        public int Board { get; set; }
+        public int SmartBoard { get; set; }
+
+        public ClassRoom()
+        {
+            
+        }
+        public ClassRoom(int id, string name, int chairs, int tables, int board, int smartBoard)
+        {
+            Id = id;
+            if(name == null)
+            {
+                throw new ArgumentException("The classRoom needs a name");
+            }
+            if (name.Length<3||name.Length>55)
+            {
+                throw new ArgumentException("The name must be between 3 and 55 charecters");
+            }
+            Name = name;
+            if(chairs < 0||tables<0)
+            {
+                throw new ArgumentException("A classroom must have chairs and tables");
+            }
+            Chairs = chairs;
+            Tables = tables;
+            Board = board;
+            SmartBoard = smartBoard;
+        }
+
 
     }
 }
